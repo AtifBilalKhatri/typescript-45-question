fbeb26cb985b604afe5aaa5c858e331447c3a2e3
AtifBilalKhatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2024-03-22 00:21:25 +0500

    Add files via upload

---
+++ 5.fanouse quote  2/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let famouse_person = "Qaid e Azam ";
+let quote = "kaam kaam or buss kaam";
+let message = `${famouse_person} always said , "${quote}"`;
+console.log(message);
---
+++ 5.fanouse quote  2/app.ts
@@ -0,0 +1,4 @@
+let famouse_person : string = "Qaid e Azam ";
+let quote : string = "kaam kaam or buss kaam";
+let message= `${famouse_person} always said , "${quote}"`;
+console.log(message);
