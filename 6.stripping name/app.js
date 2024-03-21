0006dcc5c1bb577c0ca1b2a99f421b0d83f25bfd
AtifBilalKhatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2024-03-22 00:22:42 +0500

    Add files via upload

---
+++ 6.stripping name/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = `\f\f\v\v\v\n\n\n\n\tAtif Bilal\n\n\t\f\f\v\r`;
+console.log(personName);
+const without_whitespace_characters = personName.trim();
+console.log(without_whitespace_characters);
---
+++ 6.stripping name/app.ts
@@ -0,0 +1,4 @@
+const personName: string   =`\f\f\v\v\v\n\n\n\n\tAtif Bilal\n\n\t\f\f\v`;
+console.log(personName);
+const without_whitespace_characters: string = personName.trim() ;  
+console.log(without_whitespace_characters);
