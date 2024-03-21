cb883fa62deffdb244b2c74f47a15931266c61a4
AtifBilalKhatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2024-03-22 00:26:43 +0500

    Add files via upload

---
+++ 11.names/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = [`Atif`, `Bilal`, `Abdur Rafay`, `Shabih Fatima`];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11.names/app.ts
@@ -0,0 +1,6 @@
+let names: string[] = [`Atif`,`Bilal`,`Abdur Rafay`,`Shabih Fatima`];
+
+for (let i=0; i<names.length; i++){
+    console.log(names[i])
+
+}
