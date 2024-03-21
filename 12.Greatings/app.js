e0bcea006c3f89fd76c5459ab2cf63c35953f636
AtifBilalKhatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2024-03-22 00:27:44 +0500

    Add files via upload

---
+++ 12.Greatings/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = [`Atif Bilal`, `Sumaira`, `Abdur Rafay`, `Shabih Fatima`];
+const message = `welcome to the Governer house sindh `;
+for (let i = 0; i < names.length; i++) {
+    console.log(message, names[i]);
+}
---
+++ 12.Greatings/app.ts
@@ -0,0 +1,9 @@
+let names: string[] = [`Atif Bilal`,`Sumaira`,`Abdur Rafay`,`Shabih Fatima`];
+
+const   message : string = `welcome to the Governer house sindh `;
+
+
+for (let i=0; i<names.length; i++){
+    console.log(message , names[i])
+
+}
