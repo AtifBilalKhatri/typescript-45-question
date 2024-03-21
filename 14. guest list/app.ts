9489ecd43e32bc42ee27ae0bbc606f67ad867aff
AtifBilalKhatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2024-03-22 00:29:55 +0500

    Add files via upload

---
+++ 14. guest list/app.ts
@@ -0,0 +1,9 @@
+let guestList : string[] = ['Jawwad Rajput','Zain Rajar','Zeeshan',];
+
+
+
+for (let i=0; i<guestList.length; i++ ){
+
+    console.log('Dear friend, '+ guestList[i] + '\n\n\tI am glad to invite you in my private party \n\n regards Atif \n\n\n'
+) 
+}
