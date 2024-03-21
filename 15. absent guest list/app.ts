d0f22139e1fe8c816e052adfc6078e06c6d06e16
AtifBilalKhatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2024-03-22 00:31:04 +0500

    Add files via upload

---
+++ 15. absent guest list/app.ts
@@ -0,0 +1,21 @@
+let guestList : string[] = ['Jawwad Rajput','Zain Rajar','Zeeshan',];
+
+
+
+for (let i=0; i<guestList.length; i++ ){
+
+    console.log('Dear friend, '+ guestList[i] + '\n\n\tI am glad to invite you in my private party \n\n regards Atif \n\n\n'
+) 
+}
+
+let absent_guest :string = 'Jawwad Rajput';
+let new_guest :string = 'Abdur Rafay';
+guestList[0] = new_guest
+
+for (let i=0; i<guestList.length; i++ ){
+
+    console.log('Dear friend, '+ guestList[i] + '\n\n\tI am glad to invite you in my private party \n\n regards Atif \n\n\n'
+) 
+}
+console.log(`Mr. ${absent_guest} is unable to attend our party.`);
+
