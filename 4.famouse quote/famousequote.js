f919d2bf8d4dc9b9717e9fcbba676e4b8206374d
AtifBilalKhatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2024-03-22 00:20:23 +0500

    Add files via upload

---
+++ 4.famouse quote/famousequote.js
@@ -0,0 +1,7 @@
+"use strict";
+let quote = "kaam kaam or buss kaam ";
+let auther = "Quaid e Azam";
+console.log(`${auther} always said "${quote} "`);
+let A = "this my first program and my name is ";
+let b = "atif bilal";
+console.log(`${A} ${b}`);
---
+++ 4.famouse quote/famousequote.ts
@@ -0,0 +1,8 @@
+let quote :string = "kaam kaam or buss kaam ";
+let auther :string = "Quaid e Azam";
+console.log(`${auther} always said "${quote} "`);
+
+let A : string = "this my first program and my name is ";
+let b : string = "atif bilal";
+console.log(`${A} ${b}`);
+
